90268a9ea8caf532c0c0006a2cf6ce10000bda8b
Lu Xu <lxu@autnomoustuff.com>
2017-04-23 11:34:21 -0700

    Updated model, generator works

--- model.py
+++ model.py
@@ -1,88 +1,112 @@
+import os
 import csv
 import cv2
 import numpy as np
+import sklearn
+from sklearn.utils import shuffle
 
-lines = []
-with open('./data/driving_log.csv') as f:
-    driving_data = csv.reader(f)
-    for line in driving_data: 
-        lines.append(line)
+samples = []
+i = 0
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        if i > 0 :
+            samples.append(line)
+        i = i+1
+	
+def preprocess_image(img):
+    new_img = img[50:140,:,:]
+    new_img = cv2.resize(new_img ,(200, 66), interpolation = cv2.INTER_AREA)
+    new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2YUV)
+    return new_img
 
-images = []
-steering_angles = []
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples= train_test_split(samples, test_size=0.2)
 
-for line in lines:
-    src_path = line[0]
-    filename = src_path.split('/')[-1]
-    current_path = './data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    steering_angle = float(line[3])
-    steering_angles.append(steering_angle)
+print("number of training samples: ")
+print(len(train_samples))
 
-def batch_generator(image_paths, angles, batch_size=128):
-    X, y = ([],[])
-    while True:
-        for i in range(len(angles)):
-            image = cv2.imread(image_paths[i])
-            angle = angles[i]
-            X.append(image)
-            y.append(angle)
-            if len(X) == batch_size:
-                yield (np.array(X), np.array(y))
-        
+print("\nnumber of validation samples: ")
+print(len(validation_samples))
 
-# convert to numpy arrays
-X_train = np.array(images)
-y_train = np.array(steering_angles)
+total_samples = len(train_samples) + len(validation_samples)
 
-# run through NN based on NVIDIA/JShannon model
+print("\n\ntotal number of samples should equal the original samples variable.")
+print("does it?")
+if total_samples == len(samples):
+    print("Yes")
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    image_path = './data/IMG/'
+    while 1: # Loop forever so the generator never terminates
+        small_batch_size = 32
+        shuffle(samples)
+        for offset in range(0, num_samples, small_batch_size):
+            batch_samples = samples[offset:offset+small_batch_size]
+
+            images = []
+            measurements = []
+            for batch_sample in batch_samples:
+
+                for i in range(3):
+                    source_path = './data/IMG/'+batch_sample[i]
+                    filename = source_path.split('/')[-1]
+                    current_path = 'data/IMG/' + filename
+                    image = cv2.imread(current_path)
+                    processed_image = preprocess_image(image)
+                    steering_correction = 0.2 
+                    measurement = float(batch_sample[3])
+                    if i == 1:
+                         measurement += steering_correction
+                    elif i == 2:
+                         measurement -= steering_correction
+
+                    images.append(processed_image)
+                    measurements.append(measurement)
+                    images.append(cv2.flip(processed_image,1))
+                    measurements.append(measurement*-1.0)
+
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+
+            yield sklearn.utils.shuffle(X_train, y_train)
+	
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size= 128)
+validation_generator = generator( validation_samples, batch_size=128)
+"""
+# imports for model
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Activation, Dropout, Lambda
-from keras.layers import Convolution2D, ELU
+from keras.layers import Flatten, Dense, Lambda, Dropout 
+from keras.layers.convolutional import Convolution2D, Cropping2D
+from keras.layers.advanced_activations import ELU
+from keras.callbacks import ModelCheckpoint, Callback
 from keras.regularizers import l2
 
+# model based on NVIDIA End-to-End model
 model = Sequential()
-# Normalize
-model.add(Lambda(lambda x: x/127.5 - 1.0, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(66,200,3)))
+model.add(Convolution2D(24, 5, 5,subsample = (2,2), border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
+model.add(Convolution2D(36, 5, 5,subsample = (2,2), border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
+model.add(Convolution2D(48, 5, 5,subsample = (2,2), border_mode='valid', activation="elu" , W_regularizer=l2(0.001)))
+model.add(Convolution2D(64, 3, 3, border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
+model.add(Convolution2D(64, 3, 3, border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
 
-# Three 5x5 conv layers; output depth: 24,36,48; stride: 2x2 valid
-model.add(Convolution2D(24,5,5,subsample=(2,2), border_mode='valid', W_regularizer=l2(0.001)))
-model.add(ELU())
-model.add(Convolution2D(36,5,5,subsample=(2,2), border_mode='valid', W_regularizer=l2(0.001)))
-model.add(ELU())
-model.add(Convolution2D(48,5,5,subsample=(2,2), border_mode='valid', W_regularizer=l2(0.001)))
-model.add(ELU())
-
-# Two 3x3 conv layers; output_depth:64,64
-model.add(Convolution2D(64,3,3, border_mode='valid', W_regularizer=l2(0.001)))
-model.add(ELU())
-model.add(Convolution2D(64,3,3, border_mode='valid', W_regularizer=l2(0.001)))
-model.add(ELU())
-
-# Fully Connected
 model.add(Flatten())
 
-# Three FC layers output_depth: 100, 50, 10
-model.add(Dense(100, W_regularizer=l2(0.001)))
-model.add(ELU())
-model.add(Dense(50, W_regularizer=l2(0.001)))
-model.add(ELU())
-model.add(Dense(10, W_regularizer=l2(0.001)))
-model.add(ELU())
-
-# FC output layer
+model.add(Dense(100, activation="elu", W_regularizer=l2(0.001)))
+model.add(Dense(50, activation="elu", W_regularizer=l2(0.001)))
+model.add(Dense(10, activation="elu", W_regularizer=l2(0.001)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
-#model.fit_generator(batch_generator(X_train, y_train, batch_size=BATCH_SIZE),
-#                                    samples_per_epoch=X_train.shape[0]*2,
-#                                    nb_epoch=NB_EPOCH,
-      
+checkpoint = ModelCheckpoint('model{epoch:02d}.h5')
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples) * 6,
+                    validation_data=validation_generator, nb_val_samples=len(validation_samples)*6,
+                    nb_epoch = 5, callbacks=[checkpoint])
 
-print("saving model...")
+model.save('model.h5') 
+"""
+print("Just checking data")
 
-model.save('model.h5')
-
-print("model saved!")

875c744daefd33362334f5ec8dc8df4ba2ec2566
Samuel Rustan <samrustan@gmail.com>
2017-04-25 00:01:17 -0500

    added video from run14

--- model.py
+++ model.py
@@ -4,51 +4,61 @@ import cv2
 import numpy as np
 import sklearn
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+# imports for model
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Dropout 
+from keras.layers.convolutional import Convolution2D
+from keras.layers.advanced_activations import ELU
+from keras.callbacks import ModelCheckpoint, Callback
+from keras.regularizers import l2
 
 samples = []
-i = 0
+# read in data from csv file skipping the first line
 with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
+    skipline = 0
     for line in reader:
-        if i > 0 :
+        if skipline > 0 :
             samples.append(line)
-        i = i+1
-	
+        skipline += 1
+
 def preprocess_image(img):
     new_img = img[50:140,:,:]
     new_img = cv2.resize(new_img ,(200, 66))
     new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2YUV)
     return new_img
 
-from sklearn.model_selection import train_test_split
 train_samples, validation_samples= train_test_split(samples, test_size=0.2)
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     image_path = './data/IMG/'
-    while 1: # Loop forever so the generator never terminates
-        small_batch_size = 32
-        shuffle(samples)
-        for offset in range(0, num_samples, small_batch_size):
-            batch_samples = samples[offset:offset+small_batch_size]
+    steering_correction = 0.2
 
+    while True: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
             images = []
             steering_angles = []
             for batch_sample in batch_samples:
-
-                for i in range(3):
-                    source_path = './data/IMG/'+batch_sample[i]
+                # there are 3 camera views center, left, right
+                for camera_view in range(3):
+                    source_path = image_path + batch_sample[camera_view]
                     filename = source_path.split('/')[-1]
                     current_path = 'data/IMG/' + filename
                     image = cv2.imread(current_path)
                     processed_image = preprocess_image(image)
-                    steering_correction = 0.2 
                     steering_angle = float(batch_sample[3])
-                    if i == 1:
+                    # left camera view
+                    if camera_view == 1:
                          steering_angle += steering_correction
-                    elif i == 2:
+                    # right camera view
+                    elif camera_view == 2:
                          steering_angle -= steering_correction
 
+                    # add a flipped image to the dataset to be generated
                     images.append(processed_image)
                     steering_angles.append(steering_angle)
                     images.append(cv2.flip(processed_image,1))
@@ -59,18 +69,10 @@ def generator(samples, batch_size=32):
 
             yield sklearn.utils.shuffle(X_train, y_train)
 	
-# compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size= 128)
-validation_generator = generator( validation_samples, batch_size=128)
+# create the training and validation generator objects 
+train_generator = generator(train_samples, batch_size= 32)
+validation_generator = generator(validation_samples, batch_size=32)
 
-
-# imports for model
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Dropout 
-from keras.layers.convolutional import Convolution2D, Cropping2D
-from keras.layers.advanced_activations import ELU
-from keras.callbacks import ModelCheckpoint, Callback
-from keras.regularizers import l2
 
 # model based on NVIDIA End-to-End model
 model = Sequential()
@@ -82,6 +84,8 @@ model.add(Convolution2D(24, 5, 5,subsample = (2,2), border_mode='valid', activat
 model.add(Convolution2D(36, 5, 5,subsample = (2,2), border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
 model.add(Convolution2D(48, 5, 5,subsample = (2,2), border_mode='valid', activation="elu" , W_regularizer=l2(0.001)))
 
+model.add(Dropout(0.50)
+
 # 2 more convolutional layers
 model.add(Convolution2D(64, 3, 3, border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
 model.add(Convolution2D(64, 3, 3, border_mode='valid', activation="elu", W_regularizer=l2(0.001)))
@@ -90,8 +94,11 @@ model.add(Flatten())
 
 # 3 Fully Connected layers and output layer
 model.add(Dense(100, activation="elu", W_regularizer=l2(0.001)))
+#model.add(Dropout(0.50)
 model.add(Dense(50, activation="elu", W_regularizer=l2(0.001)))
+#model.add(Dropout(0.50)
 model.add(Dense(10, activation="elu", W_regularizer=l2(0.001)))
+#model.add(Dropout(0.50)
 model.add(Dense(1))
 
 # model compiled and model checkpoints added after each epoch
